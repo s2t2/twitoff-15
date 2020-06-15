85b660f19a3541f4cd156be48cbd0e4a2f3ff99f
Mike Rossetti <s2t2@users.noreply.github.com>
2020-06-15 14:10:53 -0400

    Setup local database

--- README.md
+++ README.md
@@ -18,6 +18,17 @@ pipenv install
 pipenv shell
 ```
 
+Setup the database:
+
+```sh
+# Windows users can omit the "FLASK_APP=web_app" part...
+
+FLASK_APP=web_app flask db init #> generates app/migrations dir
+
+# run both when changing the schema:
+FLASK_APP=web_app flask db migrate #> creates the db (with "alembic_version" table)
+FLASK_APP=web_app flask db upgrade #> creates the specified tables
+```
 
 ## Usage
 
--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 
+DATABASE_URL = "sqlite:///twitoff_15_development.db" # using relative filepath
+
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     return app
